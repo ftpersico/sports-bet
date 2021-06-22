387832ac0bd040fa8111cfabb44a9c9d70c2f131
brianstauffer <brian.m.stauffer@gmail.com>
2021-06-22 19:59:26 -0400

    Initial setup

---
+++ bets.py
@@ -0,0 +1,12 @@
+
+print("Welcome to Bets 'R Us")
+print("The top sports bet aggregation app in the world")
+
+sport = input("Which sport did you want to bet on?")
+print(sport)
+
+team = input("Which team did you want to bet on?")
+print(team)
+
+bet_amount = input("How much money did you want to bet?")
+print(bet_amount)
