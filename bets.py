14cf3724b5baf00ef9d10163c5492de58fbac300
ftpersico <60077158+ftpersico@users.noreply.github.com>
2021-06-23 00:08:06 -0400

    API Odds saved in a dictionary and update reqs

--- bets.py
+++ bets.py
@@ -1,6 +1,30 @@
 import requests
 import json
+from dotenv import load_dotenv
+import os
 
+# TODO Delete this sample data after build is complete 
+sample_data = {
+  "success": "true",
+  "data": [
+    {"id": "159fb6cae00fd84860e37a3e4ebf6ba5","sport_key": "baseball_mlb", "sport_nice": "MLB",
+      "teams": ["Kansas City Royals", "New York Yankees"],
+      "home_team": "New York Yankees",
+      "commence_time": "2021-06-22T23:05:00Z",
+      "sites": [
+        {"site_key": "paddypower",
+          "site_nice": "Paddy Power",
+          "last_update": "2021-06-23T01:08:20Z",
+          "odds": {"h2h": [150,-200]}},
+        {"site_key": "betfair",
+          "site_nice": "Betfair",
+          "last_update": "2021-06-23T01:08:41Z",
+          "odds": {"h2h": [146,-172],"h2h_lay": [174,-145]}}
+      ],
+      "sites_count": 2
+    }
+  ]
+}
 
 print("Welcome to Bets 'R Us")
 print("The top sports bet aggregation app in the world")
@@ -16,35 +40,46 @@ print('Selected Team:',team)
 
 region = 'uk'
 # TODO, allow user to input a region - input("Which region are you betting in?")
-print('Selected Region:',region)
+print('Selected Region:',region.upper())
 
-# TODO pull api key from ENV file
-apiKey = '1f8a56f5813ca49ce1552283c34d8466'
+# Pull api key from ENV file
+load_dotenv()
+apiKey = os.getenv("API_KEY", default=0) 
 
 bet_amount = input("How much money did you want to bet? ")
 print(bet_amount)
 
-# URL of the Odds API
-request_url = f'https://api.the-odds-api.com/v3/odds/?apiKey={apiKey}&sport={sport}&region={region}&mkt=h2h&dateFormat=iso&oddsFormat=american'
+# TODO uncomment this when pulling from the API for real
+# request_url = f'https://api.the-odds-api.com/v3/odds/?apiKey={apiKey}&sport={sport}&region={region}&mkt=h2h&dateFormat=iso&oddsFormat=american'
+# response = requests.get(request_url)
+# print("API Status:", response.status_code)
+# all_data = json.loads(response.text)
 
+#TODO delete this when switching to real API
+all_data = sample_data
 
-response = requests.get(request_url)
-print("API Status:", response.status_code)
+# TODO only pull data for the selected team
 
-allbets = json.loads(response.text)
+# Create a dictionary with the odds for all sites
+all_odds = {}
 
-# TODO use this format to pull odds for each unique site
-sample_odds = allbets['data'][0]['sites'][0]['odds']['h2h'][0]
+for a in sample_data['data'][0]['sites']:
+    all_odds.update({
+        a['site_nice'] : a['odds']['h2h'][0]
+    })
 
-# Variables for each bet site's odds
+# TODO remove when done testing app 
+print(all_odds)
+
+# TODO Decide if we're going to use these variables for each bet site's odds
 betfair_odds = 0
-williamhill_odds = 0
+williamhill_odds = 0 
 paddypower_odds = 0
 unibet_odds = 0
 matchbook_odds = 0
 betway_odds = 0
 betfred_odds = 0
 
-print(sample_odds)
+
 
 # no input for game, will just do next game
